dd33dc05a98b14836ee9051e68a79687a136192c
jed lahrim <jolix1235@gmail.com>
2024-09-05 20:48:54 +0100

    add login.component.ts ;

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppIconComponent } from '../../../components/icons/app-icon/app-icon.component';
 import { NgIf, NgStyle } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ValidateAuthService } from '../../../validations/auth/validate-auth.service';
 import { LoaderComponent } from '../loader/loader.component';
+import { CheckboxComponent } from '../../../checkbox/checkbox.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    FormsModule,
     ReactiveFormsModule,
     RouterLink,
     NgIf,
@@ -19,6 +21,7 @@ import { LoaderComponent } from '../loader/loader.component';
     AppIconComponent,
     RouterLink,
     LoaderComponent,
+    CheckboxComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
@@ -27,6 +30,8 @@ export class LoginComponent {
   @Input({ required: true }) isLoginForm: boolean = true;
   @Input({ required: true }) headerTitle: string = 'Sign In';
   @Input({ required: true }) headerDescription: string = 'Welcom back! Please enter your details';
+  @ViewChild('checkboxRef') checkboxRef!: CheckboxComponent;
+  isChecked: boolean = false;
 
   constructor(public validateAuthService: ValidateAuthService) {
   }
@@ -38,4 +43,10 @@ export class LoginComponent {
   navigateToAccount() {
     this.isLoginForm ? this.validateAuthService.router.navigate(['/signup']) : this.validateAuthService.router.navigate(['/signin']);
   }
+
+  agreeToTerms() {
+    this.isChecked = !this.isChecked;
+    this.checkboxRef.isChecked = this.isChecked;
+  }
+
 }
